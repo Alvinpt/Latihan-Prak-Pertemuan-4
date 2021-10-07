dbd7783785db09f559237a8a8be6840ef2bdce75
Alvinpt <90598958+Alvinpt@users.noreply.github.com>
2021-10-07 12:34:20 +0800

    Add files via upload

---
+++ Latihan02_praktek03.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Latihan02_praktek03
+{
+
+public static void main(String[] args) 
+{
+
+Scanner input = new Scanner(System.in);
+
+int nilai;
+
+
+System.out.print("masukkan nilai = "); 
+nilai = input.nextInt ();
+
+
+//posisi if berjalan 
+if (nilai==1){
+
+System.out.println("angka yang dimasukkan adalah satu");
+}
+	else
+
+System.out.println("angka yang dimasukkan bukan satu");
+
+}
+
+}
---
+++ Latihan07_praktek03.java
@@ -0,0 +1,15 @@
+// Latihan mencetak angka berurut
+
+public class Latihan07_praktek03 {
+    public static void main(String args[]){
+        int i; 
+        for (i=1; i<9; i++) 
+        {
+        System.out.println(i);
+
+}
+
+
+}
+
+}
